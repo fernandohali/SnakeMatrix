9887f6344b43c2bb4e46c89a15b601399c47c23e
Fernando Hali <100550886+fernandohali@users.noreply.github.com>
2025-02-24 18:09:15 -0300

    mapeamento  a snake.

--- src/matriz/matriz.c
+++ src/matriz/matriz.c
@@ -1,16 +1,14 @@
+#include "matriz.h"
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "ws2812.pio.h"
 #include "../snake/snake.h"
-#include "../../generated/ws2812.pio.h"
-#include "matriz.h"
 
-// Variáveis globais para armazenar a cor (Entre 0 e 255 para intensidade)
-uint8_t selected_led = 0;              // Índice do LED a ser controlado (0 a 24)
-uint8_t led_r = 50;                    // Valor inicial para vermelho
-uint8_t led_g = 50;                    // Valor inicial para verde
-uint8_t led_b = 50;                    // Valor inicial para azul
-uint32_t led_buffer[NUM_PIXELS] = {0}; // Buffer para armazenar as cores de todos os LEDs
+// Buffer para armazenar as cores de todos os LEDs
+uint8_t led_r = 26; // Valor inicial para vermelho
+uint8_t led_g = 26; // Valor inicial para verde
+uint8_t led_b = 26; // Valor inicial para azul
+uint32_t led_buffer[NUM_PIXELS] = {0};
 
 // Implementações das funções
 void put_pixel(uint32_t pixel_grb)
@@ -30,6 +28,27 @@ uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b)
     return ((uint32_t)(r) << 8) | ((uint32_t)(g) << 16) | (uint32_t)(b);
 }
 
+void increase_color_intensity()
+{
+    if (led_r < 255)
+        led_r += 10;
+    if (led_g < 255)
+        led_g += 10;
+    if (led_b < 255)
+        led_b += 10;
+
+    // Diminui a intensidade do LED
+    if (led_r > 10)
+        led_r -= 5;
+    if (led_g > 10)
+        led_g -= 5;
+    if (led_b > 10)
+        led_b -= 5;
+
+    // Atualiza a matriz com as novas cores
+    update_snake();
+}
+
 void update_led_buffer()
 {
     // Apaga todos os LEDs
@@ -44,7 +63,7 @@ void update_led_buffer()
         int x = snake[i].x;
         int y = snake[i].y;
         int index = y * MATRIX_WIDTH + x;
-        led_buffer[index] = urgb_u32(0, 255, 0); // Cor verde para a cobra
+        led_buffer[index] = urgb_u32(0, 65, 0); // Cor verde para a cobra
     }
 
     // Desenha a comida
@@ -59,14 +78,4 @@ void set_leds_from_buffer()
     {
         put_pixel(led_buffer[i]);
     }
-}
-
-void increase_color_intensity()
-{
-    if (led_r < 255)
-        led_r += 10;
-    if (led_g < 255)
-        led_g += 10;
-    if (led_b < 255)
-        led_b += 10;
 }
--- src/matriz/matriz.h
+++ src/matriz/matriz.h
@@ -6,15 +6,11 @@
 #define IS_RGBW false
 #define WS2812_PIN 7
 #define NUM_PIXELS 25
+
 #define MATRIX_WIDTH 5  // Largura da matriz
 #define MATRIX_HEIGHT 5 // Altura da matriz
 
-
-
-// Declaração externa da variável global
-extern uint8_t selected_led;
-
-// Protótipos de funções
+// Função para colocar um pixel no LED correspondente
 void put_pixel(uint32_t pixel_grb);
 void put_pixel_at(int x, int y, uint32_t pixel_grb);
 uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b);
